a077b4d398bb489ecbe3cea865a23d65f044604b
Sunnyong Hur <snhjune@gmail.com>
2022-12-08 08:37:12 -0800

    first commit

---
+++ app/src/main/java/com/example/foodapp/RegistrationActivity.java
@@ -0,0 +1,99 @@
+package com.example.foodapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegistrationActivity extends AppCompatActivity {
+    EditText inputEmail, inputPassword, inputConfirmPassword,inputFullName;
+    Button btnRegister;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        inputConfirmPassword = findViewById(R.id.inputConfirmPassword);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputFullName = findViewById(R.id.inputFullName);
+        inputPassword = findViewById(R.id.inputPassword);
+        progressDialog = new ProgressDialog(this);
+        btnRegister = findViewById(R.id.button);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                performAuth();
+            }
+        });
+    }
+    private void performAuth(){
+        String fullName = inputFullName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confirmPassword = inputConfirmPassword.getText().toString();
+
+        if(!email.matches(emailPattern)){
+            inputEmail.setError("Enter Correct Email");
+
+        } else if(fullName.isEmpty()) {
+              inputFullName.setError("Full name is required");
+              inputFullName.requestFocus();
+        }
+        else if(password.isEmpty()||password.length()<4){
+            inputPassword.setError("Enter Proper password");
+        } else if (!password.equals(confirmPassword)){
+            inputConfirmPassword.setError("Password is not matched");
+        } else {
+            progressDialog.setMessage("Please wait while registration process...");
+            progressDialog.setTitle("");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegistrationActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent= new Intent(RegistrationActivity.this, HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+}
